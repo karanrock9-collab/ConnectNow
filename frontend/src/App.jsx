e599e5b4129b78fbe058cd2605e2d03438ba5c6b
Karan Khamankar <karankhamankar2@gmail.com>
2026-08-11 09:44:15 +0530

    sign in feature updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,6 +13,7 @@ function App() {
         <Routes>
           <Route path="/" element={<LandingPage />} />
           <Route path="/auth" element={<Authentication />} />
+          <Route path="/saas" element={<Authentication />} />
           <Route path="home" element={<HomeComponent />} />
           <Route path="/history" element={<History />} />
           <Route path="/:url" element={<VedioMeetComponent />} />
